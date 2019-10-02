723ccb0a1a69608eb3d9c05179855b2c6ee5e33b
Beck-Haywood <beck.haywood@students.makeschool.com>
2019-10-02 10:24:45 -0700

    finished logic behind did_survive_infection

--- person.py
+++ person.py
@@ -1,4 +1,5 @@
-import random
+from random import randint
+from numpy import random #I imported this because random.seed was giving error, this could be wrong import
 random.seed(42)
 from virus import Virus
 
@@ -18,64 +19,21 @@ class Person(object):
         self.is_alive = True  # boolean
         self.is_vaccinated = None  # boolean
         self.infection = None  # Virus object or None
-
+        self.virus = Virus('name', 0, 0)
     def did_survive_infection(self):
         ''' Generate a random number and compare to virus's mortality_rate.
         If random number is smaller, person dies from the disease.
         If Person survives, they become vaccinated and they have no infection.
         Return a boolean value indicating whether they survived the infection.
         '''
+        mortality_rate_roll = randint(0, 100)
+        if not self.infection == None:
+            if mortality_rate_roll > self.virus.mortality_rate:
+                self.is_alive = False
+            else:
+                self.is_vaccinated = True
+                self.infection = None
+        return self.is_vaccinated and self.is_alive and self.infection
         # Only called if infection attribute is not None.
         # TODO:  Finish this method. Should return a Boolean
-        pass
 
-
-''' These are simple tests to ensure that you are instantiating your Person class correctly. '''
-def test_vacc_person_instantiation():
-    # create some people to test if our init method works as expected
-    person = Person(1, True)
-    assert person._id == 1
-    assert person.is_alive is True
-    assert person.is_vaccinated is True
-    assert person.infection is None
-
-
-def test_not_vacc_person_instantiation():
-    person = Person(2, False)
-    # TODO: complete your own assert statements that test
-    # the values at each attribute
-    # assert ...
-    pass
-
-
-def test_sick_person_instantiation():
-    # Create a Virus object to give a Person object an infection
-    virus = Virus("Dysentery", 0.7, 0.2)
-    # Create a Person object and give them the virus infection
-    person = Person(3, False, virus)
-    # TODO: complete your own assert statements that test
-    # the values at each attribute
-    # assert ...
-    pass
-
-
-def test_did_survive_infection():
-    # TODO: Create a Virus object to give a Person object an infection
-    virus = Virus("Dysentery", 0.7, 0.2)
-    # TODO: Create a Person object and give them the virus infection
-    person = Person(4, False, virus)
-
-    # Resolve whether the Person survives the infection or not
-    survived = person.did_survive_infection()
-    # Check if the Person survived or not
-    if survived:
-        assert person.is_alive is True
-        # TODO: Write your own assert statements that test
-        # the values of each attribute for a Person who survived
-        # assert ...
-    else:
-        assert person.is_alive is False
-        # TODO: Write your own assert statements that test
-        # the values of each attribute for a Person who did not survive
-        # assert ...
-        pass
